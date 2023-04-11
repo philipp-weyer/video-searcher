c8dbd3debfc4e7f11fe59d466a582489961ebc73
Philipp Weyer <philipp.weyer@mongodb.com>
2023-04-11 22:22:19 +0200

    Added title and search bar

--- src/App.js
+++ src/App.js
@@ -1,24 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Button,
+  Container,
+  Row,
+  Col,
+  InputGroup,
+  DropdownButton,
+  Dropdown,
+  Form,
+  FormControl,
+  Modal,
+  Table
+} from 'react-bootstrap';
+import React, {useState, useEffect} from 'react';
+import './App.scss';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faMagnifyingGlass, faUpload } from '@fortawesome/free-solid-svg-icons';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container className="App" style={{"paddingTop": "15px"}}>
+      <Row style={{marginBottom: '20px'}}>
+        <Col align="center">
+          <p className="title">MongoDB Video Searcher</p>
+          <img src="/mongodb_logo.png" height="45px"/>
+        </Col>
+      </Row>
+      <Row>
+        <Col align="center">
+          <InputGroup className="mb-3">
+            <Button variant="outline-secondary" id="searchButton">
+              <FontAwesomeIcon size="lg" icon={faUpload} style={{marginRight:'10px'}}/>
+              Upload
+            </Button>
+            <Form.Control
+              aria-label="Text to search for"
+            />
+            <Button variant="outline-primary" id="searchButton">
+              <FontAwesomeIcon size="lg" icon={faMagnifyingGlass}/>
+            </Button>
+          </InputGroup>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
